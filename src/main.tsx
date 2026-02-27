8aa45895ef424517fa624bc9bddb23c4badc3d9c
Lordimass <lordimass@lordimass.net>
2026-02-27 14:50:50 +0000

    Reset to base Vite + React
    React router setup was the wrong thing to use and was causing issues and mess. Would rather start fresh to keep things clean.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-// src/main.tsx
-import {createRoot} from "react-dom/client";
-import {App} from "./app";
-import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const rootEl = document.getElementById("root");
-
-if (rootEl) {
-    const root = createRoot(rootEl);
-    root.render(<App />);
-} else {
-    console.error("Couldn't find root element!");
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
