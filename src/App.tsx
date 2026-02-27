a9cbc20eec67249f3b9ac4cdf58436a038e98a5c
Lordimass <lordimass@lordimass.net>
2026-02-27 15:24:55 +0000

    Installed my custom React component package (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Ticker } from "@Lordimass/lordis-react-components";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,6 +25,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Ticker inputId={""} ariaLabel={""} />
     </>
   );
 }
