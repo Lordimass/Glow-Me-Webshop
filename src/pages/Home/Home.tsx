2b7538774d9e38b2f7cd6d7b6eebf7b0ffeb1fd1
Lordimass <lordimass@lordimass.net>
2026-03-19 14:56:46 +0000

    Added Work In Progress Disclaimer and description

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,6 +20,27 @@ export default function Home() {
           />
         </div>
       </div>
+      <div className={"spacer"} />
+
+      <div></div>
+      <div className={"text-block"}>
+        <h1>Work In Progress!</h1>
+        <p>
+          We hand-craft glow in the dark models using resin and a variety of
+          different glowing powders and colours. Each has its own personality,
+          imperfections, and love put into it by us 💜
+        </p>
+        <p>
+          This website will be used to showcase and sell our models, once it's
+          set up. But for now, if you're dying to get your hands on one, you can
+          visit one of our shops in York:
+          <br />
+          <br />
+          <a href={"https://maps.app.goo.gl/VYgvgztjZpTGZHz4A"}>GHOSTS</a>
+          <br />
+          <a href={"https://maps.app.goo.gl/4K2Td9WSWWxjDewj8"}>CATS</a>
+        </p>
+      </div>
     </Page>
   );
 }
