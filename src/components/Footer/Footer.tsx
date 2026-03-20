2fc13c07972225cf8c4952475e9f52c97dd55a6c
Lordimass <lordimass@lordimass.net>
2026-03-20 08:27:10 +0000

    Fonts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
 export default function Footer() {
+  // Include credit to Abelardo Gonzalez OpenDyslexic font https://iumlpfiybqlkwoscrjzt.supabase.co/storage/v1/object/public/other-assets/Glow%20Me!/open_dyslexic/README.txt
   return <></>;
 }
